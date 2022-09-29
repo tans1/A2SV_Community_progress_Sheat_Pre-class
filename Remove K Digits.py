75d635e13fe8a940a84b9535b1e6a28ff56e7b20
tans1 <104857366+tans1@users.noreply.github.com>
2022-09-28 15:03:27 -0900

    Create  Remove K Digits.py

---
+++ Remove K Digits.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        tempStack = []
+        for i in num:
+            while (len(tempStack) != 0) and k > 0 and tempStack[-1] > i:
+                tempStack.pop()
+                k -=1
+            tempStack.append(i)
+            
+        tempStack = tempStack[:len(tempStack)-k]
+        output = ''.join(tempStack)
+        if len(output) > 0:
+            return str(int(output))
+        else:
+            return '0'
+                    
