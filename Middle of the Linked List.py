0cf599d7dad8e5a3ac9e52cf7a3cfa147baabf8d
tans1 <104857366+tans1@users.noreply.github.com>
2022-09-30 11:13:01 -0900

    Update Middle of the Linked List.py

--- Middle of the Linked List.py
+++ Middle of the Linked List.py
@@ -10,3 +10,20 @@ class Solution:
             head = head.next
             node = node.next.next   #this will travel twice the head
         return head
+    
+    ###################################################
+    # WE CAN ALSO USE THE FOLLOWING CODE
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        node = head
+        i = 0
+        while head:
+            i +=1
+            head = head.next
+        j = 0
+        while node:
+            if j == i//2:
+                return node
+            j +=1
+            node = node.next
+        return None
+    # BOTH WORKS WELL
